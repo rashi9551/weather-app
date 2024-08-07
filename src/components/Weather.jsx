45ccda3a7089477bb6608c04ccb4dbed58f9e8d8
rashi9551 <rashidroz1361@gmail.com>
2024-08-07 10:43:45 +0530

    installed tailwind and updated ui

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,105 +1,85 @@
 import  { useEffect, useRef, useState } from 'react'
-import './weather.css'
-import searchIcon from '../assets/search-icon.jpg'
-import weather from '../assets/weather1.webp'
-import Humidity from '../assets/humidity.jpg'
-import windSpeed from '../assets/windSpeed.jpg'
-import snowIcon from '../assets/snow-icon.jpg'
-import rainIcon from '../assets/rain-icon.jpg'
-import { toast } from "sonner";
-
-const Weather = () => {
-
-    const apiKey = import.meta.env.VITE_APP_ID;
-    console.log(apiKey,"sdfs");
-    const allIcon = {
-        "01d": weather,
-        "01n": weather,
-        "02d": rainIcon,
-        "02n": rainIcon,
-        "03d": rainIcon,
-        "03n": rainIcon,
-        "04d": rainIcon,
-        "04n": rainIcon,
-        "09d": rainIcon,
-        "09n": rainIcon,
-        "10d": rainIcon,
-        "10n": rainIcon,
-        "13d": snowIcon,
-        "13n": snowIcon
+import './Weather.css'
+import { toast } from 'sonner'
+function Weather() {
+    const[weatherData,setWeatherData]=useState(false)
+    const inputRef=useRef()
+    const allicons={
+        "01d":'/images/suni.png',
+         "01n":'/images/suni.png',
+          "02d":'/images/cloudi.png',
+           "02n":'/images/cloudi.png',
+            "03d":'/images/cloudi.png',
+           "03n":'/images/cloudi.png',
+           "04d":'/images/fogi.png',
+           "04n":'/images/fogi.png',
+           "09d":'/images/rainyi.png',
+           "09n":'/images/rainyi.png',
+           "10d":'/images/rainyi.png',
+           "10n":'/images/rainyi.png',
+           "13d":'/images/snowi.png',
+           "13n":'/images/snowi.png',
     }
-
-    const inputRef = useRef()
-    const [weatherData, setWeatherData] = useState(false)
-
-    const search = async (city) => {
-        if (city === "") {
+    const search=async(city)=>{
+        if(city==''){
             alert("Enter City Name")
-            return
+            return;
         }
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-            const response = await fetch(url)
-            const data = await response.json()
-
-            if (!response.ok) {
-                toast.error(data.message)
+            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
+            const response=await fetch(url)
+            const data=await response.json()
+            if(!response.ok){
+                toast.error(data.message);
+                return
             }
             console.log(data);
-            const icon = allIcon[data.weather[0].icon] || weather;
+            const icon=allicons[data.weather[0].icon] || '/images/suni.png'
             setWeatherData({
-                humidity: data.main.humidity,
-                windspeed: data.wind.speed,
-                temperature: Math.floor(data.main.temp),
-                locations: data.name,
-                icon: icon
+                humidity:data.main.humidity,
+                windSpeed:data.wind.speed,
+                temperature:Math.floor(data.main.temp),
+                location:data.name,
+                icon:icon,
+
             })
         } catch (error) {
-            setWeatherData(false)
             console.log(error);
+            setWeatherData(false)
+            
         }
     }
-
-    useEffect(() => {
-        search("Kerala")
-    }, [])
-
-
-    return (
-        <div className='weather'>
-            <div className='search-bar'>
-                <input type='text' ref={inputRef} placeholder='Search' />
-                <img src={searchIcon} alt='search' className='img' onClick={() => search(inputRef.current.value)} />
-            </div>
-
-            {weatherData ? <>
-                <img src={weatherData.icon} alt='weather' className='weather-icon' />
-                <p className='temperature'>{weatherData.temperature}°C</p>
-                <p className='location'>{weatherData.locations}</p>
-
-                <div className="weather-data">
-                    <div className="col">
-                        <img src={Humidity} alt='Humidity' className='img-humidity' />
-                        <div>
-                            <p>{weatherData.humidity}%</p>
-                            <span>Humidity</span>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <img src={windSpeed} alt='windSpeed ' className='windSpeed' />
-                        <div>
-                            <p>{weatherData.windspeed} Km/h</p>
-                            <span>Wind Speed</span>
-                        </div>
-                    </div>
-                </div>
-            </> : <>
-
-            </>}
-
-
+    useEffect(()=>{
+        search("Bengaluru")
+    },[])
+  return (
+    <div className='weather'>
+     <div className='search-bar'>
+        <input type="text" placeholder='Search...' ref={inputRef} />
+        <img src="/images/search.png" alt="" className=''onClick={()=>search(inputRef.current.value)} />
+     </div>
+     {weatherData?<> <img src={weatherData.icon}alt="" className='weather-icon'/>
+     <p className='temperature'>{weatherData.temperature}%c</p>
+     <p className='location'>{weatherData.location}</p>
+<div className='weather-data'>
+    <div className='col'>
+        <img src="/images/humidityi.png" alt="" />
+        <div>
+            <p>{weatherData.humidity}%</p>
+            <span>Humidity</span>
         </div>
-    )
+    </div>
+    <div className='col'>
+        <img src="/images/windi.png" alt="" />
+        <div>
+            <p>{weatherData.windSpeed} km/m</p>
+            <span>WInd Speed</span>
+        </div>
+    </div>
+</div></>:<></>}
+    
+    </div>
+  )
 }
 
 export default Weather
